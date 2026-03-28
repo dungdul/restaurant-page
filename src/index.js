f6de39e2a4521cd127f7e33702f32e39dbd96959
Dung Dul <178075100+dungdul@users.noreply.github.com>
2026-03-28 17:23:07 +0900

    Implement event listeners for buttons

--- src/index.js
+++ src/index.js
@@ -3,4 +3,15 @@ import renderMenuPage from'./menu.js';
 import renderAboutPage from './about.js';
 import './styles.css';
 
-renderAboutPage();
+function addEventListeners() {
+  const homebutton = document.querySelector('#home-button');
+  const menuButton = document.querySelector('#menu-button');
+  const aboutButton = document.querySelector('#about-button');
+
+  homebutton.onclick = renderHomePage;
+  menuButton.onclick = renderMenuPage;
+  aboutButton.onclick = renderAboutPage;
+}
+
+addEventListeners();
+renderHomePage();
